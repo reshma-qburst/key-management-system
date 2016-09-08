407cccea3d6070b1c012d2beb98f2c16ef7b0591
reshma <reshma@qburst.com>
2016-09-08 17:56:14 +0530

    basis project structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,1 @@
-(function() {
-    'use strict';
-    var app = angular.module('MainController', [
-        'ui.router'
-    ]);
 
-    app.config(function($stateProvider, $urlRouterProvider) {
-
-        $urlRouterProvider.otherwise('/');
-        $stateProvider
-        // HOME STATES AND NESTED VIEWS ========================================
-            .state('home', {
-            url: '/home',
-            templateUrl: 'content.html'
-        });
-
-    });
-})();
