ff05ef003bd7231800acb2ee25467395526fdc75
reshma <reshma@qburst.com>
2016-09-21 18:01:52 +0530

    modal pop up

--- assets/js/controllers/mainController.js
+++ assets/js/controllers/mainController.js
@@ -5,5 +5,41 @@
     	loadJson.getTableDefaultList().then(function(tableData) {
     		$scope.data =  tableData;
 		});
+
+		$scope.showModal = false;
+	    $scope.toggleModal = function(){
+	        $scope.showModal = !$scope.showModal;
+	    };
+
+	    $scope.isValidationModeDisabled = true;
+	    $scope.isSizeOfKeyDisabled = true;
+
+	    $scope.change = function(val) {
+	        if(val === "t1" || val === "t3"){
+	        	$scope.isValidationModeDisabled = false;
+	        }else if(val === "t2" || val === "t4"){
+	        	$scope.isValidationModeDisabled = true;
+	        }
+	    };
+
+	    $scope.changeValidationMode = function(){
+	    	$scope.isSizeOfKeyDisabled = false;
+	    };
+
+	    $scope.addDataToTable = function(){
+	    	console.log($scope.descriptionNew);
+	    	alert("Generated Key  ::: "+Math.random().toString(36).substr(2, 16));
+	    };
+
+	    $scope.beforeRender = function ($dates) {
+		    var today = new Date();
+		    var prevDate = (today.getDate()) - 1;
+		    today.setDate(prevDate);
+		    for(var d in $dates){         
+		        if($dates[d].utcDateValue<today){
+		            $dates[d].selectable = false
+		        }
+		    }  
+		}
     }]);
 })();
